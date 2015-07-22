18f803c64b45c236ea0f1794197159023b2f6a5b
Scorlater <asw0316@naver.com>
2015-07-22 10:10:54 +0000

    ver 0.01

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PjImSearch::Application.config.secret_key_base = '75602ee473523c6c47fe738bea5562b3f10f91ea3246067f39ea980e7e696655f798b9078756b4e14261f9263fceaef5102fd2b09813058ba898f1ea3e4295f0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PjImSearch::Application.config.session_store :cookie_store, key: '_pj_im_search_session'
